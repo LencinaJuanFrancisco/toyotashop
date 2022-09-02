fb9c270f188546f642a0a13b9a030d1bb732df19
LencinaJuanFrancisco <juanfranciscolencina@gmail.com>
2022-09-02 20:41:41 -0300

    se generan las card de los productos

--- src/App.js
+++ src/App.js
@@ -2,44 +2,14 @@
 
 import {Menu} from './component/Menu'
 import Heraderhero from "./component/HeaderHero";
-import CardProduct from './component/CardProduct';
+import Products from './containers/Products'
 function App() {
-  const product = [
-    {
-      bran:'Toyota',
-      type:'SEDAN',
-      imge:'/img/autoCorolla.png',
-      description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-    },
-    {
-      bran:'Corolla CROSS',
-      type:'SUV',
-      imge:'/img/autoCorollaCross.png',
-      description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-    },
-    {
-      bran:'Hillux',
-      type:'Camioneta',
-      imge:'/img/autoHilluk.png',
-      description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-    },
-    {
-      bran:'Prious',
-      type:'Electrico',
-      imge:'/img/autoPRIUS.png',
-      description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-    }
-  ]
+ 
   return (
     <div className="App">
       <Menu/>
       <Heraderhero/>
-      {
-        product.forEach(pro => {
-          <CardProduct product={pro}/>
-          
-        })
-      }
+      <Products/>
     </div>
   );
 }
--- src/component/CardProduct.js
+++ src/component/CardProduct.js
@@ -1,26 +1,22 @@
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
-function CradProduct({product}) {
+function CradProduct({ product }) {
   return (
-    
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={product.img} />
-      <Card.Body>
-        <Card.Title>{product.bran}</Card.Title>
-        <Card.Text>
-        {product.description}
-        </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroup.Item>{product.type}</ListGroup.Item>
-       
-      </ListGroup>
-      <Card.Body>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
-    </Card>
+   
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={product.imge} />
+        <Card.Body>
+          <Card.Title>{product.bran}</Card.Title>
+          <Card.Text>{product.description}</Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+        <Card.Body>
+          <Card.Link href="#">Comprar</Card.Link>
+          <Card.Link href="#">Ver mas</Card.Link>
+        </Card.Body>
+      </Card>
+   
   );
 }
 
